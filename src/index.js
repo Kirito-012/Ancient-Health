a2128464d4cf0e7ad429dbed4a098eb56c2d37c2
Kirito-012 <11.uknowwho@gmail.com>
2026-02-07 14:08:07 +0530

    bugs

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
+if (!Array.prototype.toReversed) {
+	// eslint-disable-next-line no-extend-native
+	Array.prototype.toReversed = function () {
+		return Array.from(this).reverse()
+	}
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
